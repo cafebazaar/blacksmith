cafebazaar/blacksmith

commit 947f76493fa7b324f20069d8184ff3da1debcd2f

Changing the utility for embedding assets
`go-bindata-assetfs` seemed to be the cause of some panics. So I replaced
`go-bindata` and `go-bindata-assetfs` with `esc`.
http://mattjibson.com/blog/2014/11/19/esc-embedding-static-assets/

+ Improving the structure of `Makefile`
+ HTTP Logging for API

pxe/http_booter.go
@@ -34,13 +34,13 @@ type nodeContext struct {
 
 type HTTPBooter struct {
 	listenAddr     net.TCPAddr
-	ldlinux        []byte
+	ldlinux        http.File
 	key            [32]byte
 	runtimeConfig  *datasource.RuntimeConfiguration
 	bootParamsRepo *cloudconfig.Repo
 }
 
-func NewHTTPBooter(listenAddr net.TCPAddr, ldlinux []byte, runtimeConfig *datasource.RuntimeConfiguration, bootParamsRepo *cloudconfig.Repo) (*HTTPBooter, error) {
+func NewHTTPBooter(listenAddr net.TCPAddr, ldlinux http.File, runtimeConfig *datasource.RuntimeConfiguration, bootParamsRepo *cloudconfig.Repo) (*HTTPBooter, error) {
 	booter := &HTTPBooter{
 		listenAddr:     listenAddr,
 		ldlinux:        ldlinux,
@@ -62,10 +62,9 @@ func (b *HTTPBooter) Mux() *http.ServeMux {
 }
 
 func (b *HTTPBooter) ldlinuxHandler(w http.ResponseWriter, r *http.Request) {
-	logging.Debug("HTTPBOOTER", "starting send of ldlinux.c32 to %s (%d bytes)", r.RemoteAddr, len(b.ldlinux))
+	logging.LogHTTPRequest("HTTPBOOTER", r)
 	w.Header().Set("Content-Type", "application/octet-stream")
-	w.Write(b.ldlinux)
-	logging.Log("HTTPBOOTER", "sent ldlinux.c32 to %s (%d bytes)", r.RemoteAddr, len(b.ldlinux))
+	io.Copy(w, b.ldlinux)
 }
 
 func (b *HTTPBooter) pxelinuxConfig(w http.ResponseWriter, r *http.Request) {
@@ -184,7 +183,8 @@ func (b *HTTPBooter) fileHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func HTTPBooterMux(listenAddr net.TCPAddr, runtimeConfig *datasource.RuntimeConfiguration, bootParamsRepo *cloudconfig.Repo) (*http.ServeMux, error) {
-	ldlinux, err := Asset("pxelinux/ldlinux.c32")
+	pxelinuxDir := FS(false)
+	ldlinux, err := pxelinuxDir.Open("/pxelinux/ldlinux.c32")
 	if err != nil {
 		return nil, err
 	}

pxe/tftp.go
@@ -1,6 +1,7 @@
 package pxe
 
 import (
+	"io"
 	"net"
 
 	"github.com/cafebazaar/blacksmith/logging"
@@ -8,11 +9,17 @@ import (
 )
 
 func ServeTFTP(listenAddr net.UDPAddr) error {
-	pxelinux, err := Asset("pxelinux/lpxelinux.0")
+	pxelinuxDir := FS(false)
+	pxelinux, err := pxelinuxDir.Open("/pxelinux/lpxelinux.0")
 	if err != nil {
 		return err
 	}
 	tftp.Log = func(msg string, args ...interface{}) { logging.Log("TFTP", msg, args...) }
 	tftp.Debug = func(msg string, args ...interface{}) { logging.Debug("TFTP", msg, args...) }
-	return tftp.ListenAndServe("udp4", listenAddr.String(), tftp.Blob(pxelinux))
+
+	handler := func(string, net.Addr) (io.ReadCloser, error) {
+		return pxelinux, nil
+	}
+
+	return tftp.ListenAndServe("udp4", listenAddr.String(), handler)
 }

Makefile
@@ -3,6 +3,19 @@ COMMIT := $(shell git rev-parse HEAD)
 BUILD_TIME := $(shell LANG=en_US date +"%F_%T_%z")
 DOCKER_IMAGE ?= "cafebazaar/blacksmith"
 
+help:
+	@echo "Please use \`make <target>' where <target> is one of"
+	@echo "  blacksmith   to build the main binary (for linux/amd64)"
+	@echo "  docker       to build the docker image"
+	@echo "  push         to push the built docker to docker hub"
+	@echo "  test         to run unittests"
+	@echo "  clean        to remove generated files"
+
+define run-generate =
+	go get github.com/mjibson/esc
+	GOOS=linux GOARCH=amd64 go generate
+endef
+
 test: *.go */*.go pxe/pxelinux_autogen.go web/ui_autogen.go
 	go get -t -v ./...
 	go test -v ./...
@@ -12,12 +25,10 @@ blacksmith: *.go */*.go pxe/pxelinux_autogen.go web/ui_autogen.go
 	GOOS=linux GOARCH=amd64 go build -ldflags "-s -X main.version=$(VERSION) -X main.commit=$(COMMIT) -X main.buildTime=$(BUILD_TIME)" -o blacksmith
 
 pxe/pxelinux_autogen.go: pxe/pxelinux
-	go get github.com/jteeuwen/go-bindata/...
-	GOOS=linux GOARCH=amd64 go generate
+	$(run-generate)
 
 web/ui_autogen.go: web/ui
-	go get github.com/jteeuwen/go-bindata/...
-	GOOS=linux GOARCH=amd64 go generate
+	$(run-generate)
 
 clean:
 	rm -f blacksmith pxe/pxelinux_autogen.go web/ui_autogen.go
@@ -28,4 +39,4 @@ docker: blacksmith
 push:
 	docker push $(DOCKER_IMAGE)
 
-.PHONY: clean docker push test
+.PHONY: help clean docker push test

blacksmith.go
@@ -19,8 +19,8 @@ import (
 	etcd "github.com/coreos/etcd/client"
 )
 
-//go:generate go-bindata -o pxe/pxelinux_autogen.go -prefix=pxe -pkg pxe -ignore=README.md pxe/pxelinux
-//go:generate go-bindata -o web/ui_autogen.go -pkg web web/ui/...
+//go:generate esc -o pxe/pxelinux_autogen.go -prefix=pxe -pkg pxe -ignore=README.md pxe/pxelinux
+//go:generate esc -o web/ui_autogen.go -prefix=web -pkg web web/ui
 
 var _ cloudconfig.DataSource = (*datasource.RuntimeConfiguration)(nil)
 var _ cloudconfig.DataSource = (*datasource.Flags)(nil)

datasource/runtime_configuration.go
@@ -44,7 +44,7 @@ func NewRuntimeConfiguration(dataSource etcd.KeysAPI, etcdClient etcd.Client, et
 		return nil, errors.New("A valid initial CoreOS version is required in initial data")
 	}
 
-	fmt.Printf("Initial Values: CoreOSVersion=%s\n", iVals.CoreOSVersion)
+	fmt.Printf("Initial Values:  CoreOSVersion=%s\n", iVals.CoreOSVersion)
 
 	instance := &RuntimeConfiguration{
 		DataSource:           dataSource,

logging/logging.go
@@ -1,6 +1,9 @@
 package logging // import "github.com/cafebazaar/blacksmith/logging"
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 const logFormat = "%c[%s] %s"
 
@@ -39,4 +42,18 @@ func Log(subsystem string, msg string, args ...interface{}) {
 // Debug wrtites Debug level logs to logger
 func Debug(subsystem string, msg string, args ...interface{}) {
 	logCh <- logEntry{subsystem, true, fmt.Sprintf(msg, args...)}
+}
+
+func httpRequestMsg(r *http.Request) string {
+	return fmt.Sprintf("%s RemoteAddr:%s Method:%s Referer:%s UserAgent:%s", r.URL, r.RemoteAddr, r.Method, r.Referer(), r.UserAgent())
+}
+
+// LogHTTPRequest wrtites Info level logs of HTTP requests to logger
+func LogHTTPRequest(subsystem string, r *http.Request) {
+	logCh <- logEntry{subsystem, false, httpRequestMsg(r)}
+}
+
+// DebugHTTPRequest wrtites Debug level logs of HTTP requests to logger
+func DebugHTTPRequest(subsystem string, r *http.Request) {
+	logCh <- logEntry{subsystem, true, httpRequestMsg(r)}
 }

web/server.go
@@ -11,11 +11,14 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/elazarl/go-bindata-assetfs"
-
 	"github.com/cafebazaar/blacksmith/datasource"
 	"github.com/cafebazaar/blacksmith/dhcp"
+	"github.com/cafebazaar/blacksmith/logging"
 	"github.com/gorilla/mux"
+)
+
+const (
+	debugTag = "API"
 )
 
 type RestServer struct {
@@ -30,6 +33,8 @@ type uploadedFile struct {
 }
 
 func (a *RestServer) deleteFile(w http.ResponseWriter, r *http.Request) {
+	logging.LogHTTPRequest(debugTag, r)
+
 	name := r.FormValue("name")
 
 	if name != "" {
@@ -47,6 +52,8 @@ func (a *RestServer) deleteFile(w http.ResponseWriter, r *http.Request) {
 }
 
 func (a *RestServer) files(w http.ResponseWriter, r *http.Request) {
+	logging.LogHTTPRequest(debugTag, r)
+
 	files, err := ioutil.ReadDir(filepath.Join(a.runtimeConfig.WorkspacePath, "files"))
 	if err != nil {
 		http.Error(w, err.Error(), 500)
@@ -69,6 +76,8 @@ func (a *RestServer) files(w http.ResponseWriter, r *http.Request) {
 }
 
 func (a *RestServer) upload(w http.ResponseWriter, r *http.Request) {
+	logging.LogHTTPRequest(debugTag, r)
+
 	const MaxFileSize = 1 << 30
 	// This feels like a bad hack...
 	if r.ContentLength > MaxFileSize {
@@ -122,12 +131,14 @@ func (a *RestServer) Mux() *mux.Router {
 	mux.HandleFunc("/files", a.files).Methods("GET")
 	mux.HandleFunc("/files", a.deleteFile).Methods("DELETE")
 	mux.PathPrefix("/files/").Handler(http.StripPrefix("/files/", http.FileServer(http.Dir(filepath.Join(a.runtimeConfig.WorkspacePath, "files")))))
-	mux.PathPrefix("/ui/").Handler(http.StripPrefix("/ui/", http.FileServer(&assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, Prefix: "/web/ui"})))
+	mux.PathPrefix("/ui/").Handler(http.FileServer(FS(false)))
 
 	return mux
 }
 
 func (a *RestServer) nodesList(w http.ResponseWriter, r *http.Request) {
+	logging.LogHTTPRequest(debugTag, r)
+
 	leases, err := a.pool.Leases()
 	if err != nil {
 		http.Error(w, "Error in fetching lease data", 500)
@@ -141,7 +152,8 @@ func (a *RestServer) nodesList(w http.ResponseWriter, r *http.Request) {
 }
 
 func (a *RestServer) etcdEndpoints(w http.ResponseWriter, r *http.Request) {
-	// a.runtimeConfig.
+	logging.LogHTTPRequest(debugTag, r)
+
 	endpointsJSON, err := json.Marshal(a.runtimeConfig.EtcdClient.Endpoints())
 	if err != nil {
 		io.WriteString(w, fmt.Sprintf("{'error': %s}", err))

commit 92e9e452b6da297887579c63d8faef2a23625ba3

Fixing a bug introduced in 947f76493
The `File` objects of the embedded files were read once, but wasn't reset automatically.
So, the boots from the second time were broken.

pxe/http_booter.go
@@ -1,7 +1,6 @@
 package pxe
 
 import (
-	"crypto/rand"
 	"fmt"
 	"io"
 	"net"
@@ -35,22 +34,18 @@ type nodeContext struct {
 
 type HTTPBooter struct {
 	listenAddr          net.TCPAddr
-	ldlinux             http.File
-	key                 [32]byte
+	ldlinux             []byte
 	datasource          datasource.GeneralDataSource
 	bootParamsTemplates *template.Template
 }
 
-func NewHTTPBooter(listenAddr net.TCPAddr, ldlinux http.File,
+func NewHTTPBooter(listenAddr net.TCPAddr, ldlinux []byte,
 	ds datasource.GeneralDataSource, bootParamsTemplates *template.Template) (*HTTPBooter, error) {
 	booter := &HTTPBooter{
 		listenAddr:          listenAddr,
 		ldlinux:             ldlinux,
 		datasource:          ds,
 		bootParamsTemplates: bootParamsTemplates,
-	}
-	if _, err := io.ReadFull(rand.Reader, booter.key[:]); err != nil {
-		return nil, fmt.Errorf("cannot initialize ephemeral signing key: %s", err)
 	}
 	return booter, nil
 }
@@ -66,7 +61,7 @@ func (b *HTTPBooter) Mux() *http.ServeMux {
 func (b *HTTPBooter) ldlinuxHandler(w http.ResponseWriter, r *http.Request) {
 	logging.LogHTTPRequest("HTTPBOOTER", r)
 	w.Header().Set("Content-Type", "application/octet-stream")
-	io.Copy(w, b.ldlinux)
+	w.Write(b.ldlinux)
 }
 
 func (b *HTTPBooter) pxelinuxConfig(w http.ResponseWriter, r *http.Request) {
@@ -188,7 +183,7 @@ func (b *HTTPBooter) fileHandler(w http.ResponseWriter, r *http.Request) {
 
 func HTTPBooterMux(listenAddr net.TCPAddr, ds datasource.GeneralDataSource,
 	bootParamsTemplates *template.Template) (*http.ServeMux, error) {
-	ldlinux, err := FS(false).Open("/pxelinux/ldlinux.c32")
+	ldlinux, err := FSByte(false, "/pxelinux/ldlinux.c32")
 	if err != nil {
 		return nil, err
 	}

pxe/tftp.go
@@ -10,14 +10,14 @@ import (
 
 func ServeTFTP(listenAddr net.UDPAddr) error {
 	pxelinuxDir := FS(false)
-	pxelinux, err := pxelinuxDir.Open("/pxelinux/lpxelinux.0")
-	if err != nil {
-		return err
-	}
 	tftp.Log = func(msg string, args ...interface{}) { logging.Log("TFTP", msg, args...) }
 	tftp.Debug = func(msg string, args ...interface{}) { logging.Debug("TFTP", msg, args...) }
 
 	handler := func(string, net.Addr) (io.ReadCloser, error) {
+		pxelinux, err := pxelinuxDir.Open("/pxelinux/lpxelinux.0")
+		if err != nil {
+			return nil, err
+		}
 		return pxelinux, nil
 	}
 
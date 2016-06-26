67f2ef1a0ac781e1ef9364ec6b1120a4472647fc
mehdy <mehdy.khoshnoody@gmail.com>
2016-06-26 13:57:20 +0430

    changed clients ip range

--- install-as-docker.sh
+++ install-as-docker.sh
@@ -46,7 +46,7 @@ IP=$(ip addr | awk '/inet/ && /'$INTERFACE'/{sub(/\/.*$/,"",$2); print $2}')
 IFS=. read -r i1 i2 i3 i4 <<< "$IP"
 [ "$i4" -lt 21 ] || echo "Warning: Your IP will be among those the dhcp is going to assign to the nodes! (Detected IP: $IP)" 1>&2
 
-LEASE_START="$i1.$i2.$i3.21"
+LEASE_START="$i1.$i2.$i3.31"
 LEASE_RANGE=244
 LEASE_SUBNET="255.255.255.0"
 ROUTER="$i1.$i2.$i3.1"

8aeb0826cce080b0b37b2830294ef9918d3476e4
Touhid Arastu <touhid.arastu@gmail.com>
2016-08-01 14:14:00 +0430

    add shared functions to utils package

--- blacksmith.go
+++ blacksmith.go
@@ -16,6 +16,7 @@ import (
 	"github.com/cafebazaar/blacksmith/dhcp"
 	"github.com/cafebazaar/blacksmith/logging"
 	"github.com/cafebazaar/blacksmith/pxe"
+	"github.com/cafebazaar/blacksmith/utils"
 	"github.com/cafebazaar/blacksmith/web"
 	etcd "github.com/coreos/etcd/client"
 )
@@ -70,34 +71,6 @@ func init() {
 	}
 }
 
-func interfaceIP(iface *net.Interface) (net.IP, error) {
-	addrs, err := iface.Addrs()
-	if err != nil {
-		return nil, err
-	}
-	fs := [](func(net.IP) bool){
-		net.IP.IsGlobalUnicast,
-		net.IP.IsLinkLocalUnicast,
-		net.IP.IsLoopback,
-	}
-	for _, f := range fs {
-		for _, a := range addrs {
-			ipaddr, ok := a.(*net.IPNet)
-			if !ok {
-				continue
-			}
-			ip := ipaddr.IP.To4()
-			if ip == nil {
-				continue
-			}
-			if f(ip) {
-				return ip, nil
-			}
-		}
-	}
-	return nil, fmt.Errorf("interface %s has no usable unicast addresses", iface.Name)
-}
-
 func gracefulShutdown(etcdDataSource datasource.DataSource) {
 	err := etcdDataSource.RemoveInstance()
 	if err != nil {
@@ -143,7 +116,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	serverIP, err := interfaceIP(dhcpIF)
+	serverIP, err := utils.InterfaceIP(dhcpIF)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "\nError while trying to get the ip from the interface (%v)\n", dhcpIF)
 		os.Exit(1)

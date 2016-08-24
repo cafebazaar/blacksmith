1fdc363ede9227b80e0053a5841afad750ec671e
Reza Mohammadi <reza@cafebazaar.ir>
2016-08-24 17:06:27 +0430

    Dependency upgrade: pixiecore is moved and heavily changed

--- pxe/tftp.go
+++ pxe/tftp.go
@@ -5,31 +5,49 @@ import (
 	"net"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/danderson/pixiecore/tftp"
+	"go.universe.tf/netboot/tftp"
 )
 
+// ServeTFTP delivers lpxelinux.0 to machines through the old tftp protocol
 func ServeTFTP(listenAddr net.UDPAddr) error {
 	pxelinuxDir := FS(false)
-	tftp.Logf = func(msg string, args ...interface{}) {
-		log.WithFields(log.Fields{
-			"where":  "pxe.ServeTFTP",
-			"action": "tftp-said",
-		}).Infof(msg, args...)
-	}
-	tftp.Debug = func(msg string, args ...interface{}) {
-		log.WithFields(log.Fields{
-			"where":  "pxe.ServeTFTP",
-			"action": "tftp-said",
-		}).Debugf(msg, args...)
-	}
 
-	handler := func(string, net.Addr) (io.ReadCloser, error) {
+	handler := func(string, net.Addr) (io.ReadCloser, int64, error) {
 		pxelinux, err := pxelinuxDir.Open("/pxelinux/lpxelinux.0")
 		if err != nil {
-			return nil, err
+			return nil, 0, err
 		}
-		return pxelinux, nil
+		stat, err := pxelinux.Stat()
+		if err != nil {
+			return nil, 0, err
+		}
+		return pxelinux, stat.Size(), nil
 	}
 
-	return tftp.ListenAndServe("udp4", listenAddr.String(), handler)
+	tftpServer := &tftp.Server{
+		Handler: handler,
+		InfoLog: func(msg string) {
+			log.WithFields(log.Fields{
+				"where":  "pxe.ServeTFTP",
+				"action": "tftp",
+			}).Infof(msg)
+		},
+		TransferLog: func(clientAddr net.Addr, path string, err error) {
+			if err != nil {
+				log.WithError(err).WithFields(log.Fields{
+					"where":   "pxe.ServeTFTP",
+					"action":  "tftp-transfer",
+					"subject": path,
+				}).Warnf("error while transfering to %s", clientAddr.String())
+			} else {
+				log.WithFields(log.Fields{
+					"where":   "pxe.ServeTFTP",
+					"action":  "tftp-transfer",
+					"subject": path,
+				}).Debugf("transfered to %s", clientAddr.String())
+			}
+		},
+	}
+
+	return tftpServer.ListenAndServe(listenAddr.String())
 }

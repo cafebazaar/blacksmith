6b4aa7fcd75a615e1da43e4b55022d9ad739b117
Ebrahim Byagowi <ebrahim@gnu.org>
2016-10-22 11:53:51 +0330

    Check uploaded workspace validity and minor refactor

--- utils/tar.go
+++ utils/tar.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"archive/tar"
+	"crypto/md5"
+	"encoding/hex"
 	"io"
 	"os"
 	"path/filepath"
@@ -44,4 +46,35 @@ func Untar(tarball, target string) error {
 		}
 	}
 	return nil
+}
+
+// http://www.mrwaggel.be/post/generate-md5-hash-of-a-file/
+func HashFileMD5(filePath string) (string, error) {
+	// Initialize variable returnMD5String now in case an error has to be returned
+	var returnMD5String string
+
+	// Open the passed argument and check for any error
+	file, err := os.Open(filePath)
+	if err != nil {
+		return returnMD5String, err
+	}
+
+	// Tell the program to call the following function when the current function returns
+	defer file.Close()
+
+	// Open a new hash interface to write to
+	hash := md5.New()
+
+	// Copy the file in the hash interface and check for any error
+	if _, err := io.Copy(hash, file); err != nil {
+		return returnMD5String, err
+	}
+
+	// Get the 16 bytes hash
+	hashInBytes := hash.Sum(nil)[:16]
+
+	// Convert the bytes to a string
+	returnMD5String = hex.EncodeToString(hashInBytes)
+
+	return returnMD5String, nil
 }

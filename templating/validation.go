baa79bdb456a47e8f1a1c151c5a15a3feaa1db82
Sina Siadat <siadat@gmail.com>
2017-01-07 14:22:01 +0330

    Refactor ValidateCloudConfig for readability

--- templating/validation.go
+++ templating/validation.go
@@ -7,13 +7,11 @@ import (
 )
 
 func ValidateCloudConfig(config string) string {
-	var errors bytes.Buffer
-	errors.WriteString("\n")
+	var buf bytes.Buffer
+	buf.WriteString("\n")
 	validationReport, _ := validate.Validate([]byte(config))
 	for _, errorEntry := range validationReport.Entries() {
-		errors.WriteString("#")
-		errors.WriteString(errorEntry.String())
-		errors.WriteString("\n")
+		buf.WriteString("# " + errorEntry.String() + "\n")
 	}
-	return errors.String()
+	return buf.String()
 }

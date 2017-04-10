fd7db8262a84ec4cfbe19930a55ed7d8caaac453
Sina Siadat <siadat@gmail.com>
2017-04-10 16:23:43 +0430

    sciprts: add install and reboot nodes scripts

---
+++ scripts/install-nodes.bash
@@ -0,0 +1,9 @@
+./blacksmithctl update workspaces
+
+./blacksmithctl set node-key 00:02:7d:15:be:82 state bootstrapper1
+./blacksmithctl set node-key 00:02:7d:15:be:84 state bootstrapper2
+./blacksmithctl set node-key 00:02:7d:15:be:86 state bootstrapper3
+
+./blacksmithctl install node 00:02:7d:15:be:82
+./blacksmithctl install node 00:02:7d:15:be:84
+./blacksmithctl install node 00:02:7d:15:be:86
---
+++ scripts/reboot-nodes.bash
@@ -0,0 +1,3 @@
+./blacksmithctl reboot node 00:02:7d:15:be:82
+./blacksmithctl reboot node 00:02:7d:15:be:84
+./blacksmithctl reboot node 00:02:7d:15:be:86
